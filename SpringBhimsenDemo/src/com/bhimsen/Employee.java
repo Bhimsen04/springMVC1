6724965777f3182bc1986154955e17a15c27aed6
Bhimsen <bhimsen@healthsignz.com>
2020-08-12 15:22:30 +0530

    Dependenct Injection Implemented

--- SpringBhimsenDemo/src/com/bhimsen/Employee.java
+++ SpringBhimsenDemo/src/com/bhimsen/Employee.java
@@ -3,13 +3,19 @@ package com.bhimsen;
 //POJO or Model or Bean
 public class Employee {
 
-	//Attributes
+	// Attributes
 	int eid;
 	String ename;
 	double esalary;
+	Address address;
 
 	public Employee() {
 		System.out.println("Employee object constructed");
+	}
+
+	// Constructor injection
+	public Employee(Address address) {
+		this.address = address;
 	}
 
 	public Employee(int eid, String ename, double esalary) {
@@ -42,9 +48,18 @@ public class Employee {
 		this.esalary = esalary;
 	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	// Setter injection
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
 	@Override
 	public String toString() {
-		return "Employee [eid=" + eid + ", ename=" + ename + ", esalary=" + esalary + "]";
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esalary=" + esalary + ", address=" + address + "]";
 	}
 
 }
